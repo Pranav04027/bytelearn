df30c9c211de3013986978f37c2153695e21298b
Pranav Chauhan <pranavchauhan040@gmail.com>
2025-06-22 20:54:27 +0530

    feat: added dashboard routes and controller

--- src/app.js
+++ src/app.js
@@ -35,6 +35,7 @@
     import commentRouter from "./routes/comment.routes.js"
     import tweetRouter from "./routes/tweet.routes.js"
     import likeRouter from "./routes/like.routes.js"
+    import dashboardRouter from "./routes/dashboard.routers.js"
     import { errorHandler } from "./middlewares/error.middlewares.js"
 
 
@@ -47,6 +48,7 @@
     app.use("/api/v1/comments", commentRouter)
     app.use("/api/v1/tweets", tweetRouter)
     app.use("/api/v1/likes", likeRouter)
+    app.use("/api/v1/dashboard", dashboardRouter)
     
 
 
